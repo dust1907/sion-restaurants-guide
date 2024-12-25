61de16dbbb890720c1882d5befe20978a87aa4e6
dust1907 <ffogl.ai@gmail.com>
2024-12-25 10:36:54 +0100

    Add restaurant menus data

---
+++ src/data/menus.js
@@ -0,0 +1,197 @@
+export const restaurantMenus = {
+  "la-sitterie": {
+    name: "La Sitterie",
+    sections: [
+      {
+        name: "Entrées",
+        items: [
+          {
+            name: "Foie Gras Maison",
+            description: "Foie gras mi-cuit, chutney de figues et brioche toastée",
+            price: "32 CHF",
+            isSignature: true
+          },
+          {
+            name: "Velouté de Potimarron",
+            description: "Aux éclats de châtaignes et huile de truffe",
+            price: "18 CHF"
+          },
+          {
+            name: "Tartare de Féra du Léman",
+            description: "Aux herbes fraîches et citron vert",
+            price: "26 CHF"
+          }
+        ]
+      },
+      {
+        name: "Plats Principaux",
+        items: [
+          {
+            name: "Filet de Boeuf du Valais",
+            description: "Sauce aux morilles, pommes duchesse et légumes de saison",
+            price: "58 CHF",
+            isSignature: true
+          },
+          {
+            name: "Carré d'Agneau en Croûte d'Herbes",
+            description: "Jus au thym, gratin dauphinois",
+            price: "48 CHF"
+          },
+          {
+            name: "Omble Chevalier",
+            description: "Beurre blanc aux agrumes, risotto aux champignons",
+            price: "45 CHF"
+          }
+        ]
+      }
+    ]
+  },
+  "la-terrasse": {
+    name: "La Terrasse",
+    sections: [
+      {
+        name: "Entrées",
+        items: [
+          {
+            name: "Carpaccio de Noix de Saint-Jacques",
+            description: "Huile d'olive vierge et citron",
+            price: "29 CHF",
+            isSignature: true
+          },
+          {
+            name: "Salade de Chèvre Chaud",
+            description: "Miel du Valais et noix caramélisées",
+            price: "24 CHF"
+          }
+        ]
+      },
+      {
+        name: "Plats",
+        items: [
+          {
+            name: "Tournedos Rossini",
+            description: "Foie gras poêlé et sauce au Madère",
+            price: "65 CHF",
+            isSignature: true
+          },
+          {
+            name: "Filets de Perche Meunière",
+            description: "Pommes allumettes et sauce tartare maison",
+            price: "42 CHF"
+          }
+        ]
+      }
+    ]
+  },
+  "cafe-des-chateaux": {
+    name: "Café des Châteaux",
+    sections: [
+      {
+        name: "À Partager",
+        items: [
+          {
+            name: "Planchette Valaisanne",
+            description: "Sélection de viandes séchées et fromages locaux",
+            price: "32 CHF",
+            isSignature: true
+          },
+          {
+            name: "Focaccia aux Herbes",
+            description: "Huile d'olive et fleur de sel",
+            price: "12 CHF"
+          }
+        ]
+      },
+      {
+        name: "Plats",
+        items: [
+          {
+            name: "Burger des Châteaux",
+            description: "Bœuf du Valais, raclette, oignons caramélisés",
+            price: "29 CHF",
+            isSignature: true
+          },
+          {
+            name: "Rösti Traditionnel",
+            description: "Œuf au plat et fromage râpé",
+            price: "24 CHF"
+          }
+        ]
+      }
+    ]
+  },
+  "le-coq-en-pate": {
+    name: "Le Coq en Pâte",
+    sections: [
+      {
+        name: "Entrées",
+        items: [
+          {
+            name: "Terrine de Foie Gras Maison",
+            description: "Pain d'épices et confiture d'oignons",
+            price: "28 CHF"
+          },
+          {
+            name: "Escargots de Bourgogne",
+            description: "Beurre persillé",
+            price: "22 CHF",
+            isSignature: true
+          }
+        ]
+      },
+      {
+        name: "Plats",
+        items: [
+          {
+            name: "Coq au Vin",
+            description: "Garniture grand-mère, pommes mousseline",
+            price: "39 CHF",
+            isSignature: true
+          },
+          {
+            name: "Magret de Canard",
+            description: "Sauce aux fruits rouges, polenta crémeuse",
+            price: "45 CHF"
+          }
+        ]
+      }
+    ]
+  },
+  "le-resto-verso": {
+    name: "Le Resto-Verso",
+    sections: [
+      {
+        name: "Entrées",
+        items: [
+          {
+            name: "Tartare de Bœuf",
+            description: "Préparé minute, condiments maison",
+            price: "26 CHF",
+            isSignature: true
+          },
+          {
+            name: "Soupe à l'Oignon",
+            description: "Gratinée au fromage d'alpage",
+            price: "16 CHF"
+          }
+        ]
+      },
+      {
+        name: "Plats",
+        items: [
+          {
+            name: "Entrecôte Parisienne",
+            description: "Beurre café de Paris, frites maison",
+            price: "46 CHF",
+            isSignature: true
+          },
+          {
+            name: "Suprême de Volaille",
+            description: "Sauce aux morilles, gratin de cardons",
+            price: "38 CHF"
+          }
+        ]
+      }
+    ]
+  }
+};
